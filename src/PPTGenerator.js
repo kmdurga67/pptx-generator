5b672c9a7be8a0c3c12b07ab103a8417f2df60a3
Kumari Durga <singhdurga5698@gmail.com>
2023-11-13 18:10:28 +0530

    corrected indentation

--- src/PPTGenerator.js
+++ src/PPTGenerator.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import pptxgen from 'pptxgenjs';
+import React, { useState } from "react";
+import pptxgen from "pptxgenjs";
 
 const PPTGenerator = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [imageFile, setImageFile] = useState(null);
 
   const generatePPT = () => {
     if (!title || !imageFile) {
-      alert('Title and Image are required!');
+      alert("Title and Image are required!");
       return;
     }
 
@@ -18,45 +18,48 @@ const PPTGenerator = () => {
     titleSlide.addText(title, {
       x: 2,
       y: 2,
-      w: '100%',
+      w: "100%",
       h: 1.5,
-      align: 'center',
+      align: "center",
       fontSize: 44,
-      color: '#00008B',
+      color: "#00008B",
       bold: true,
-      italic:true
+      italic: true,
     });
 
     const maxCharsPerSlide = 680;
     let remainingDescription = description;
 
     while (remainingDescription.length > 0) {
-      const currentDescription = remainingDescription.substring(0, maxCharsPerSlide);
+      const currentDescription = remainingDescription.substring(
+        0,
+        maxCharsPerSlide
+      );
       remainingDescription = remainingDescription.substring(maxCharsPerSlide);
 
       const descriptionSlide = pptx.addSlide();
       descriptionSlide.addText(currentDescription, {
         x: 1,
         y: 1,
-        w: '80%',
+        w: "80%",
         h: 3,
         fontSize: 16,
-        color: '#AA336A',
-        italic:true,
-        align:'justify',
+        color: "#AA336A",
+        italic: true,
+        align: "justify",
       });
     }
 
     const imageSlide = pptx.addSlide();
     imageSlide.addImage({
-      path: imageFile, 
+      path: imageFile,
       x: 2,
       y: 1,
       w: 6,
       h: 4,
     });
 
-    pptx.writeFile({ fileName: 'presentation' });
+    pptx.writeFile({ fileName: "presentation" });
   };
 
   const handleImageChange = (e) => {
@@ -66,11 +69,16 @@ const PPTGenerator = () => {
 
   return (
     <div className="container mx-auto my-8 text-center">
-      <h1 className="text-4xl font-bold mb-6 text-blue-500">PPT Generator Application</h1>
+      <h1 className="text-4xl font-bold mb-6 text-blue-500">
+        PPT Generator Application
+      </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="bg-white p-6 rounded-lg shadow-md">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
             Your Title:
           </label>
           <input
@@ -84,7 +92,10 @@ const PPTGenerator = () => {
         </div>
 
         <div className="bg-white p-6 rounded-lg shadow-md">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
+          >
             Add Description:
           </label>
           <textarea
@@ -98,7 +109,10 @@ const PPTGenerator = () => {
       </div>
 
       <div className="bg-white p-6 rounded-lg shadow-md mt-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imageFile">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="imageFile"
+        >
           Add Image:
         </label>
         <input
